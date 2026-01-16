e40a7314a8da776e6ddd6f310df2083a4ece5e86
IndahPS-99 <240030272@stikom-bali.ac.id>
2026-01-16 14:20:13 +0700

    tambah fitur login di auth.py

--- auth.py
+++ auth.py
@@ -1,1 +1,30 @@
+from datetime import datetime, timedelta
+from typing import Optional
 
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+
+SECRET_KEY = "ganti_dengan_secret_yang_panjang_dan_sulit_ditebak"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
+
+# GANTI ke sha256_crypt dulu biar tidak kena batas 72 byte bcrypt
+pwd_context = CryptContext(schemes=["sha256_crypt"], deprecated="auto")
+
+def get_password_hash(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode_token(token: str):
+    try:
+        return jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+    except JWTError:
+        return None
